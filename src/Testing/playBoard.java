7511cc39f16fd62a2d9068f718f10f606154ed5d
Gonzalo Hitta <dezolator94@gmail.com>
2025-08-27 13:44:33 -0300

    fix contructor for playboard

--- src/Testing/playBoard.java
+++ src/Testing/playBoard.java
@@ -1,19 +1,23 @@
 package Testing;
 
 public class playBoard {
-	enum cells {isPainted , isX}
-	String playBoard[][] ;
-	
-	
-	public playBoard(int size) {
-		for (int i=0;i<size;i++) {
-			for(int j=0;j<size;j++) {
-				if(Math.random()<0.5) {
-				playBoard[i][j]="isPainted";}
-			
-				else playBoard[i][j]="isX";
-			}
-		
-		}
-	}
+    enum Cells { IS_PAINTED, IS_X }
+    private String[][] board;
+
+    public playBoard(int size) {
+        board = new String[size][size]; 
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (Math.random() < 0.5) {
+                    board[i][j] = Cells.IS_PAINTED.name();
+                } else {
+                    board[i][j] = Cells.IS_X.name();
+                }
+            }
+        }
+    }
+
+    public String[][] getBoard() {
+        return board;
+    }
 }
