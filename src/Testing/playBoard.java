2ad8686a2ede96aa812373845835e0bdb8b420c9
Gonzalo Hitta <dezolator94@gmail.com>
2025-08-28 17:22:23 -0300

    added initializeRandom() method
    added initrandom al final

--- src/Testing/playBoard.java
+++ src/Testing/playBoard.java
@@ -98,4 +98,12 @@ public class playBoard {
     public void setCell(int row, int col, Cells value) {
         board[row][col] = value;
     }
+    public void initializeRandom() {
+        for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    board[i][j] = Math.random() < 0.5 ? Cells.IS_PAINTED : Cell.IS_X;
+                }
+        }
+    }
+    //--------//
 }
