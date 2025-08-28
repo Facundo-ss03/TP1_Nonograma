5749e0ac327f3ac93d44d305c5cded4f35b25f98
Gonzalo Hitta <dezolator94@gmail.com>
2025-08-28 12:26:20 -0300

    Complete the class with methods, added hintsFuntion
    *refactor.
    *use enums.
    *added getters and setters
    *added hint collector method

--- src/Testing/playBoard.java
+++ src/Testing/playBoard.java
@@ -1,23 +1,101 @@
 package Testing;
 
-public class playBoard {
-    enum Cells { IS_PAINTED, IS_X }
-    private String[][] board;
+import java.util.ArrayList;
+import java.util.List;
 
+public class playBoard {
+    enum Cells { IS_PAINTED, IS_X, IS_BLANK }
+    private Cells[][] board;
+
+    
     public playBoard(int size) {
-        board = new String[size][size]; 
+        createBlankBoard(size);
+    }
+
+    private void createBlankBoard(int size) {
+        board = new Cells[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (Math.random() < 0.5) {
-                    board[i][j] = Cells.IS_PAINTED.name();
-                } else {
-                    board[i][j] = Cells.IS_X.name();
-                }
+                board[i][j] = Cells.IS_BLANK;
             }
         }
     }
 
-    public String[][] getBoard() {
+    
+    public List<Integer> getRowHints(int row) {
+        List<Integer> hints = new ArrayList<>();
+        int count = 0;
+
+        for (int j = 0; j < board[row].length; j++) {
+            if (board[row][j] == Cells.IS_PAINTED) {
+                count++;
+            } else {
+                if (count > 0) {
+                    hints.add(count);
+                    count = 0;
+                }
+            }
+        }
+        if (count > 0) { 
+            hints.add(count);
+        }
+
+        if (hints.isEmpty()) {
+            hints.add(0); 			// 0  no painted cells
+        }
+
+        return hints;
+    }
+
+    public List<Integer> getColumnHints(int col) {
+        List<Integer> hints = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == Cells.IS_PAINTED) {
+                count++;
+            } else {
+                if (count > 0) {
+                    hints.add(count);
+                    count = 0;
+                }
+            }
+        }
+        if (count > 0) {
+            hints.add(count);
+        }
+
+        if (hints.isEmpty()) {
+            hints.add(0);
+        }
+
+        return hints;
+    }
+
+    
+    public Cells[][] getBoard() {
         return board;
+    }
+    
+    public void toggleCell(int row, int col) {
+        switch (board[row][col]) {
+            case IS_BLANK:
+                board[row][col] = Cells.IS_PAINTED;
+                break;
+            case IS_PAINTED:
+                board[row][col] = Cells.IS_X;
+                break;
+            case IS_X:
+                board[row][col] = Cells.IS_BLANK;
+                break;
+        }
+    }
+
+    public Cells getCell(int row, int col) {
+        return board[row][col];
+    }
+
+    public void setCell(int row, int col, Cells value) {
+        board[row][col] = value;
     }
 }
