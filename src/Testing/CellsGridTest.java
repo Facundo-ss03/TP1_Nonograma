c4f0728ec30f48982f3c9335898f755112d75e8f
Facundo Sosa <facundomartinsosa205@gmail.com>
2025-09-01 16:15:21 -0300

    test de Celltest correctos

--- src/Testing/CellsGridTest.java
+++ src/Testing/CellsGridTest.java
@@ -1,10 +1,30 @@
 package Testing;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import Model.CellsGrid;
 
 public class CellsGridTest {
+	
+	int size;
+	CellsGrid aleatoryCellsGrid;
+	CellsGrid inBlankCellsGrid;
+	int[] hintOfBlackCell;
+	
+	@Before
+	public void init() {
+		
+		size = 5;
+		aleatoryCellsGrid = new CellsGrid(size);
+		inBlankCellsGrid = CellsGrid.createInBlankCopyOfCellsGrid(aleatoryCellsGrid);
+		
+		hintOfBlackCell = aleatoryCellsGrid.getHintOfBlackCell();
+		
+	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void createInvalidCellsGridTest() {
@@ -24,6 +44,30 @@ public class CellsGridTest {
 	public void createCellsGrid() {
 		
 		CellsGrid grid = new CellsGrid(5);
+		
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void getHintOfBlackCellOfInBlankCellsGridTest() {
+		
+		inBlankCellsGrid.getHintOfBlackCell();
+		
+	}
+	
+	@Test
+	public void getHintOfBlackCellOfAleatoryCellsGridTest() {
+		
+		int[] hint = new int[2];
+		assertEquals(hint.length, aleatoryCellsGrid.getHintOfBlackCell().length);
+		
+	}
+	
+	@Test
+	public void hintInRangeTest() {
+
+		boolean validHint = hintOfBlackCell[0] < size && hintOfBlackCell[0] >= 0;
+		
+		assertTrue(validHint);
 		
 	}
 	
